0a26861cde1bb920d8246a720dccbfc5849e71ce
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2024-07-08 14:39:49 +0900

    first commit

---
+++ src/test/java/com/boot/passbatchpractice/PassBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.passbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
